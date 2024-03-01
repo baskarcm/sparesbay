eab1f4b8c369acf4adaa5ce76a6095463ecaae46
arunkumar1002 <arun.techzar@gmail.com>
2024-03-01 10:07:01 +0530

    Logo changed

--- src/components/header/Logo.tsx
+++ src/components/header/Logo.tsx
@@ -1,10 +1,12 @@
 // react
-import React from 'react';
+import React from "react";
 // third-party
-import { FormattedMessage } from 'react-intl';
+import { FormattedMessage } from "react-intl";
 // application
-import AppLink from '~/components/shared/AppLink';
-import url from '~/services/url';
+import AppLink from "~/components/shared/AppLink";
+import url from "~/services/url";
+import LOGO from "../../../public/images/LOGO.png";
+import Image from "next/legacy/image";
 
 interface Props extends React.HTMLAttributes<HTMLElement> {}
 
@@ -17,7 +19,7 @@ function Logo(props: Props) {
                 </div>
                 <div className="logo__image">
                     {/* logo */}
-                    <svg xmlns="http://www.w3.org/2000/svg" width="168" height="26">
+                    {/* <svg xmlns="http://www.w3.org/2000/svg" width="168" height="26">
                         <path
                             className="logo__part-primary"
                             d="M50,26h-5c-1.1,0-2-0.9-2-2V2c0-1.1,0.9-2,2-2h5c6.6,0,12,5.4,12,12v2C62,20.6,56.6,26,50,26z M57,12
@@ -49,7 +51,15 @@ c0.1,0.1,0.2,0.2,0.3,0.3c0.3,0.3,0.5,0.7,0.8,1.1c0.1,0.1,0.1,0.2,0.2,0.3C83,4.8,
 c0,0.2,0.1,0.4,0.1,0.6c0,0.2,0.1,0.4,0.1,0.6C83.9,8,84,8.2,84,8.4c0,0.2,0,0.4,0,0.6s0,0.4,0,0.6C84,9.8,83.9,10,83.9,10.2z M75,5
 h-4c-0.6,0-1,0.4-1,1v6c0,0.6,0.4,1,1,1h4c2.2,0,4-1.8,4-4S77.2,5,75,5z"
                         />
-                    </svg>
+                    </svg> */}
+                    <Image
+                        src={LOGO}
+                        alt="logo"
+                        className="newLogo mt-1"
+                        id="myImage"
+                        // minheight={"95%"}
+                        // minwidth={"95%"}
+                    />
                     {/* logo / end */}
                 </div>
             </AppLink>
